5a9a60a4469f46c11ea452e05e3d05eb3e01a315
Andre Merdan <andre.merdan@aoe.com>
2020-09-04 15:07:17 +0200

    fix types

--- src/NumberLock/index.stories.tsx
+++ src/NumberLock/index.stories.tsx
@@ -6,16 +6,14 @@ import { Button } from "@material-ui/core";
 import { NumberLock, Props } from ".";
 
 export default {
-  title: "Component/NumberLock",
-  component: NumberLock,
-  argTypes: {
-    // backgroundColor: { control: 'color' },
-  },
+	title: "Component/NumberLock",
+	component: NumberLock,
+	argTypes: {},
 } as Meta;
 
 const Template: Story<Props> = (args) => <NumberLock {...args} />;
 
 export const Basic = Template.bind({});
 Basic.args = {
-  component: Button,
+	Component: Button,
 };
--- src/NumberLock/index.tsx
+++ src/NumberLock/index.tsx
@@ -1,33 +1,33 @@
-import React from 'react'
-import { Button as ButtonReakit } from 'reakit/Button'
+import React from "react";
+import { Button as ButtonReakit, ButtonHTMLProps } from "reakit/Button";
 
 export interface Props {
-  handleResult?: (result: number[]) => void
-  component?: React.Component<React.HTMLProps<HTMLButtonElement>>
+	handleResult?: (result: number[]) => void;
+	Component?: React.ComponentType<ButtonHTMLProps | any>;
 }
 
-export const NumberLock: React.FC<Props> = ({ handleResult, component }) => {
-  const Button = component || ButtonReakit
-  const [state, setState] = React.useState([] as number[])
-  const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
-  React.useEffect(() => {
-    handleResult && handleResult(state)
-  }, [handleResult, state])
+export const NumberLock: React.FC<Props> = ({ handleResult, Component }) => {
+	const Button = Component || ButtonReakit;
+	const [state, setState] = React.useState([] as number[]);
+	const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+	React.useEffect(() => {
+		handleResult && handleResult(state);
+	}, [handleResult, state]);
 
-  const handleOnClick = (e: React.MouseEvent<HTMLButtonElement>) => {
-    setState([...state, parseInt(e.currentTarget.value)])
-  }
+	const handleOnClick = (e: React.MouseEvent<HTMLButtonElement>) => {
+		setState([...state, parseInt(e.currentTarget.value)]);
+	};
 
-  return (
-    <>
-      {numbers.map((value) => (
-        <React.Fragment key={value}>
-          <Button onClick={handleOnClick} value={value}>
-            {value}
-          </Button>
-          {value % 3 === 0 && <div />}
-        </React.Fragment>
-      ))}
-    </>
-  )
-}
+	return (
+		<>
+			{numbers.map((value) => (
+				<React.Fragment key={value}>
+					<Button onClick={handleOnClick} value={value}>
+						{value}
+					</Button>
+					{value % 3 === 0 && <div />}
+				</React.Fragment>
+			))}
+		</>
+	);
+};
