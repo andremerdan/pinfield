c303e9f000fe3037630a4550e5348562c4e0cc02
Andre Merdan <andre.merdan@aoe.com>
2020-10-28 08:41:57 +0100

    update react

--- src/components/NumberLock/NumberLock.tsx
+++ src/components/NumberLock/NumberLock.tsx
@@ -8,16 +8,20 @@ export interface Props {
   Component?: React.ComponentType<any>;
 }
 
-export const NumberLock: React.FC<Props> = ({ handleResult, Component }) => {
+export const NumberLock: React.FC<Props> = ({
+  // handleResult
+  Component,
+}) => {
   const Button = Component || ButtonReakit;
-  const [state, setState] = React.useState([] as number[]);
+  // const [state, setState] = React.useState([] as number[]);
   const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
-  React.useEffect(() => {
-    handleResult && handleResult(state);
-  }, [handleResult, state]);
+  // React.useEffect(() => {
+  //   handleResult && handleResult(state);
+  // }, [handleResult, state]);
 
   const handleOnClick = (e: React.MouseEvent<HTMLButtonElement>) => {
-    setState([...state, parseInt(e.currentTarget.value)]);
+    console.log(e);
+    // setState([...state, parseInt(e.currentTarget.value)]);
   };
 
   return (
